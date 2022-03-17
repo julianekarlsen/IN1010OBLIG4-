56bf7b98ea9701541540b6d7ebd422650e0dcca9
julianekarlsen <101723421+julianekarlsen@users.noreply.github.com>
2022-03-17 12:49:58 +0100

    Add files via upload

--- Oblig4/Legesystemer.java
+++ Oblig4/Legesystemer.java
@@ -1,20 +1,63 @@
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Legesystemer extends LenkeListe<Lege> {
-    public static void main(String [] args) {
-        
-    public Legesystemer(){
+    
+    protected int linjeNr = 0;
+    protected String linje;
+    protected String dataType;
+
+    private IndeksertListe<Pasient> pasientListe = new IndeksertListe<Pasient>();
+    private IndeksertListe<Lege> legeListe = new IndeksertListe<Lege>();
+    private IndeksertListe<Legemiddler> legemiddelListe = new IndeksertListe<Legemiddler>();
 
 
-    } 
-
-    public void lesInnFil(""){
+    public void lesInnFil(String filnavn){
 
 
+        try{
+            Scanner nyFil = new Scanner(new File(filnavn));
 
-    }
+                while(nyFil.hasNextLine()){
+                    String lin = nyFil.nextLine();
+                    linjeNr++;
+                    String[] linje = lin.split(" ");
+
+                    if(lin.contains("#")){
+                        dataType;
+                        dataType = linje[1];
+    
+                        }
+            
+                }
+                
+
+
+
+        //       String lin = nyFil.nextLine();.
+        //       System.out.println(lin);
+        //       String[] linje = lin.split(" ");
+      
+        //       int antallNoder = Integer.parseInt(linje[0]);
+        //       int antProsessorer = Integer.parseInt(linje[1]);
+        //       int minnePrN = Integer.parseInt(linje[2]);
+      
+        //       for (int i = 0; i < antallNoder; i++){
+        //         settInnNode(new Node(minnePrN, antProsessorer));
+        //       }
+        nyFil.close();
+
+      
+          }catch (Exception e){
+             System.out.println("Kan ikke lese " + filnavn + "!");
+             System.out.println(e.getMessage());           // om man ikke finner filen
+           }
+
+
+
+
+
+
     
  }
 }
